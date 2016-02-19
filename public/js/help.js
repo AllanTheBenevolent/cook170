acea3757d48f0a0d789f19a0540de1e88b2b64eb
Chau Lucky Nguyen <lkn007@ucsd.edu>
2016-02-18 22:38:08 -0800

    lucky - fixed help page instructions, reverted submit back

--- public/js/help.js
+++ public/js/help.js
@@ -1,18 +1,27 @@
-$('li#1').click(clickHelp1);
-$('li#2').click(clickHelp2);
-$('li#3').click(clickHelp3);
+$('a.right').click(clickRight);
+$('a.left').click(clickLeft);
 
+var rightCount = 0;
+var leftCount = 0;
 
-function clickHelp1(e) {
-    $('img.speechbubble').attr('src', 'img/help.buddystep1.png');
+function clickRight(e) {
+    rightCount +=1;
+    
+    if(rightCount == 1){
+         $('img.speechbubble').attr('src', 'img/help.buddystep2.png');
+    }
+    else if(rightCount == 2){
+         $('img.speechbubble').attr('src', 'img/help.buddystep3.png');
+    }
 }
 
-function clickHelp2(e) {
-    $('img.speechbubble').attr('src', 'img/help.buddystep2.png');
+function clickLeft(e) {
+    leftCount +=1;
+    if(leftCount == 1){
+         $('img.speechbubble').attr('src', 'img/help.buddystep2.png');
+    }
+    else if(leftCount == 2){
+         $('img.speechbubble').attr('src', 'img/help.buddystep1.png');
+    }
 }
-
-function clickHelp3(e) {
-    $('img.speechbubble').attr('src', 'img/help.buddystep3.png');
-}
-
-
+   
--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,8 +1,22 @@
+$('input.solid').click(clickAdd); // remove if you want to use the above code
+var count = 0;
+
+function clickAdd(e){
+    count += 1;
+    if(count == 1){
+        $('img.speechbubble').attr("src", "img/yumhomepage.png");
+    }
+    else if(count == 2){
+        count = 0;
+        $('img.speechbubble').attr("src", "img/defaulthomepage.png");
+    } 
+}
+
 // TRY THE UNCOMMENTED CODE FIRST TO SEE WHAT SHOULD HAPPEN (and the error that occurs mentioned below) 
 
 //ORIGINAL PROBLEM (CURRENT UNCOMMENTED CODE): yellow submit button will only show when you click 'ADD' button with no values in the field. When I do add actual items into the field, the submit button reverts back to gray! To solve the issue, I added code (that is currently commented out) to disable the 'ADD' button until the field is nonempty in hope that the submit button will only work after the user inputs items.
 
-//CURRENT PROBLEM (COMMENTED CODE + UNCOMMENTED CODE TOGETHER): if you uncomment everything, all the buttons are disabled until the field is nonempty (yay!) but now the click listener will not work when you click 'ADD'. when you click 'ADD', the 'SUBMIT' button should turn yellow and allow you to click on it. POSSIBLE BUG: It is not working because once you click 'ADD' when you input a value into the field, the field becomes nonempty again, which resets everything. Because of this, the click listener to enable and change the color of the 'SUBMIT' button will never work. 
+/*//CURRENT PROBLEM (COMMENTED CODE + UNCOMMENTED CODE TOGETHER): if you uncomment everything, all the buttons are disabled until the field is nonempty (yay!) but now the click listener will not work when you click 'ADD'. when you click 'ADD', the 'SUBMIT' button should turn yellow and allow you to click on it. POSSIBLE BUG: It is not working because once you click 'ADD' when you input a value into the field, the field becomes nonempty again, which resets everything. Because of this, the click listener to enable and change the color of the 'SUBMIT' button will never work. 
 
 //disable submit button when it is gray (default). button is enabled when it is yellow after you input values into the field
 $('a#submit').bind('click', false);
@@ -34,7 +48,7 @@ $('a#submit').click(function (event) {
         }
     });
 })()*/
-
+/*
 $('input.solid').click(clickAdd); // remove if you want to use the above code
 var count = 0;
 
@@ -55,4 +69,4 @@ function clickAdd(e){
         $('img.submit').attr("src", "img/submit.png");
         $('a#submit').unbind('click', false);
     } 
-}
+}*/

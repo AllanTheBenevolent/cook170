3c3e0afc051ac14ca03c77149a1f3d1ae8b4f5e8
Ryan Weng <rweng@ucsd.edu>
2016-02-18 22:53:42 -0800

    json for user, profile, and edit profile

--- routes/editProfile.js
+++ routes/editProfile.js
@@ -1,7 +1,9 @@
+var data = require("../users.json");
+
 /*
  * GET editProfile page.
  */
 
 exports.editProfilePage = function(req, res){
-  res.render('editProfile');
+  res.render('editProfile', {currentuser:data["users"]});
 };
--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,9 @@
+var data = require("../users.json");
+
 /*
  * GET profile page.
  */
 
 exports.profilePage = function(req, res){
-  res.render('profile');
+  res.render('profile', {currentuser:data["users"]});
 };

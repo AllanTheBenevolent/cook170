7d99911dc7f086714fd6addcc1192ff11fa815ea
Allan Wong <alw040@ucsd.edu>
2016-02-23 02:18:46 -0800

    Made it where adding food would append on top of the list instead of below it. Attempted to put bookmarks but ran into a weird problem of it not displaying at all

--- routes/add.js
+++ routes/add.js
@@ -5,7 +5,7 @@ exports.addItem = function(req, res){
    var newItem = {
         "name": req.query.itemName
 	}
-
-    data["items"].push(newItem);
+    data["items"].unshift(newItem);
 	res.render('add',{userItems:data["items"]});
+
 };

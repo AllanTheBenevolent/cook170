509563c61e9b8352166d9837231b412f68542d26
Chau Lucky Nguyen <lkn007@ucsd.edu>
2016-02-29 22:27:56 -0800

    lucky, fixed a lot of css and added event listeners

--- public/js/bookmarkaction.js
+++ public/js/bookmarkaction.js
@@ -13,6 +13,17 @@ $('button.bookmark').click(function(){
 	console.log(send);
     
 	$.post('/bookmarked', send , function (res) {});
-    $(this).text('bookmarked');
+    var friendName = clickBookmark($(this).text());
+	console.log($(this).text());
+	$(this).text(friendName);
 
 });
+
+function clickBookmark(e){
+    if (e == 'Bookmark'){
+        return 'Bookmarked';
+    }
+    else if (e == 'Bookmarked'){
+        return 'Bookmark';
+    }
+}
